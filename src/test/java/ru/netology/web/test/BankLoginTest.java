e9f3179da6fda2d3d8f0e1c75c2c88196bb62b71
Svetlana <sve1656@yandex.ru>
2024-06-20 15:25:11 +0300

    добавлены тесты

---
+++ src/test/java/ru/netology/web/test/BankLoginTest.java
@@ -0,0 +1,64 @@
+package ru.netology.web.test;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.data.SQLHelper;
+import ru.netology.web.page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.web.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.web.data.SQLHelper.cleanDatabase;
+
+
+public class BankLoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+
+    @BeforeEach
+    public void setup() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+
+    }
+
+
+    @Test
+    void successFullLogin() {
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void getErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    void getErrorNotificationIfLoginWithExistUserAndRandomVerificationCode(){
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+
+}
